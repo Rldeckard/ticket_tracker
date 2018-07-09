4e8c68c953bd975f4899caeb37124946b16ae1bf
Ryan Deckard <ryan.l.deckard@gmail.com>
2018-07-08 20:53:45 -0500

    add sum

--- ticket.php
+++ ticket.php
@@ -28,15 +28,13 @@ if (strlen($_SESSION['username']) < 1) {
     <div style="background-color:lightgray;position:relative;top:-26px;padding-bottom:30px">
       <hr>
       <h2 style="padding:5px">Ticket View</h2>
-      <div style="padding-right:20px;text-align:right;">
-        <a style="background-color:darkgray;text-decoration:none;color:black;padding:10px;border-radius:10px" href=" <?php print $path; ?>/create_ticket.php" >Create New Ticket</a>                                            
-      </div>
       <div style="background-color:gray;margin:40px;border-radius:20px;box-shadow:5px 5px 10px 5px;">
       <?php
         $data_q = mysqli_query($conn, "select * from tickets.summary WHERE ID =".$_GET['ID']);
         if (mysqli_num_rows($data_q) > 0) {
           while ($r = mysqli_fetch_assoc($data_q)) {
-            print $r['description'];
+            print "Summary: ".$r['summary'];
+            print "Comments: ".$r['description'];
 
           }
         }
