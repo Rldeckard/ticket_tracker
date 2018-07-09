b162577d786e5adfe8165d06b9a41a69cb7ac612
Ryan Deckard <ryan.l.deckard@gmail.com>
2018-07-08 22:10:00 -0500

    added table limit

--- index.php
+++ index.php
@@ -10,18 +10,18 @@ if (strlen($_SESSION['username']) < 1) {
   <title>935TH S6 Ticket Tracker</title>
   <a style="text-align:right" href="logout.php">Logout</a>
   <style>
-    
+
     tr:hover {
       background-color: darkgray;
     }
-    td:hover { 
+    td:hover {
       curser: hand;
     }
     tr {
       background-color: lightgray;
-      
+
     }
-    
+
   </style>
 <body>
   <div>
@@ -30,7 +30,7 @@ if (strlen($_SESSION['username']) < 1) {
       <hr>
       <h2 style="padding:5px">Ticket View</h2>
       <div style="padding-right:20px;text-align:right;">
-        <a style="background-color:darkgray;text-decoration:none;color:black;padding:10px;border-radius:10px" href=" <?php print $path; ?>/create_ticket.php" >Create New Ticket</a>                                            
+        <a style="background-color:darkgray;text-decoration:none;color:black;padding:10px;border-radius:10px" href=" <?php print $path; ?>/create_ticket.php" >Create New Ticket</a>
       </div>
       <div style="background-color:gray;margin:40px;border-radius:20px;box-shadow:5px 5px 10px 5px;">
       <?php
@@ -43,11 +43,11 @@ if (strlen($_SESSION['username']) < 1) {
         <table style="width:100%;border:1px black solid;border-collapse:collapse;border-radius:15px">
         <tr style="border:1px black solid;">
 <?php   foreach ($columns as $col) {
-          if ($col != 'id' && $col != 'priority') { 
+          if ($col != 'id' && $col != 'priority') {
             print '<th style="padding:5px;color:white;background-color:black">'.ucfirst($col).'</th>';
             $r_col[] = $col;
           }
-        } 
+        }
 ?>
         </tr>
 <?php   $data_q = mysqli_query($conn, "select * from tickets.summary;");
@@ -57,23 +57,23 @@ if (strlen($_SESSION['username']) < 1) {
 <?php
             foreach ($r_col as $col) {
               switch ($r[$col]) {
-                case 'Active': $color = '#0AAD3B'; break; 
+                case 'Active': $color = '#0AAD3B'; break;
                 case 'Waiting': $color = '#bcbc0f'; break;
                 case '1': $color = 'darkred'; $r[$col] = ''; break; //set urgencies
                 default: $color = ''; break; //sets default table color to gray
               }
-              
-              print '<td style="text-align:center;border:1px black solid;padding:10px;background-color:'.$color.'">'.$r[$col].'</td>';
-            } 
+
+              print '<td style="text-align:center;border:1px black solid;padding:10px;max-width:200px;overflow:hidden;background-color:'.$color.'">'.$r[$col].'</td>';
+            }
             print '</tr>';
           }
         }
 ?>
-        
+
         </table>
         </div>
       </div>
-    </div>  
+    </div>
   </div>
 </body>
 </html>
